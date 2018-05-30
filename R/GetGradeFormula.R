0da488ada88f7cf70edf2e81217be775c7245aae
Lucasg6 <39525568+Lucasg6@users.noreply.github.com>
2018-05-30 16:26:00 +0200

    Update GetGradeFormula.R
    Rajout de condition sur les arguments

--- R/GetGradeFormula.R
+++ R/GetGradeFormula.R
@@ -15,6 +15,9 @@
 #' GetGradeFormula(5, 10, coefs, max = 3)
 #' GetGradeFormula(5, 10, coefs, round = NA) 
 GetGradeFormula <- function(n, m, coefs, max = NA, round = 0.5) {
+  
+  stopifnot(coefs > 0, n > 0 && n%%1 == 0, m > 0 && m%%1 == 0, round > 0 || round == NA, max > 0 || && max == NA, max <= length(coefs))
+  
   input <- Idx2Ref(c(rbind(n, m + seq_along(coefs) - 1)))
   if(is.numeric(max)) {
     input <- paste("MAX(", input, ", ", Idx2Ref(c(n, m + max - 1)), ")", sep = "")
