7fa9ef00bb83329bf931502954c84292ed25f9ff
Lucasg6 <39525568+Lucasg6@users.noreply.github.com>
2018-05-30 17:11:06 +0200

    Update GetStatFormula.R

--- R/GetStatFormula.R
+++ R/GetStatFormula.R
@@ -14,7 +14,7 @@
 #' 
 GetStatFormula <- function(n, m, nmin = 2) {
   
-  stopifnot(n > 0 && n%%1 == 0, m > 0 && m%%1 == 0, nmin > 0 && nmin%%1 == 0)
+  stopifnot((n > 0 && nmin > 0) || (nmin == 0 && nmin == n), n%%1 == 0 && nmin%%1 == 0, m > 0 && m%%1 == 0)
   
   sprintf(
     "%s(%s%s)",
